f803f5dd2fe7deace87a2d9a34fe86b35660624b
abiakarn <nora.abiakar@gmail.com>
2018-05-28 13:45:31 +0200

    Refactored functions in cprinter and partition by constraint

--- modcc/printer/cprinter.cpp
+++ modcc/printer/cprinter.cpp
@@ -28,6 +28,26 @@ void emit_simd_procedure_proto(std::ostream&, ProcedureExpression*, const std::s
 void emit_api_body(std::ostream&, APIMethod*);
 void emit_simd_api_body(std::ostream&, APIMethod*, moduleKind);
 
+void emit_indexed_simd_state_reads(std::ostream &out, const std::vector<LocalVariable *> &indexed_vars,
+                                   simd_expr_constraint constraint);
+
+void emit_indexed_simd_state_updates(std::ostream &out, const std::vector<LocalVariable *> &indexed_vars,
+                                     const simd_expr_constraint &constraint);
+
+void emit_index_initialize(std::ostream &out, const std::unordered_set<std::string> &indices,
+                           simd_expr_constraint constraint);
+
+void emit_body_for_loop(std::ostream &out, BlockExpression *body, const std::vector<LocalVariable *> &indexed_vars,
+                   const std::unordered_set<std::string> &indices, const simd_expr_constraint &read_constraint,
+                   const simd_expr_constraint &write_constraint);
+
+void emit_for_loop_per_constraint(std::ostream &out, BlockExpression *body,
+                                  const std::vector<LocalVariable *> &indexed_vars,
+                                  const std::unordered_set<std::string> &indices,
+                                  const simd_expr_constraint &read_constraint,
+                                  const simd_expr_constraint &write_constraint,
+                                  std::string underlying_constraint_name);
+
 struct cprint {
     Expression* expr_;
     explicit cprint(Expression* expr): expr_(expr) {}
@@ -40,29 +60,25 @@ struct cprint {
 
 struct simdprint {
     Expression* expr_;
-    bool is_var_indexed_;
-    bool is_contiguous;
-    bool is_constant;
+    bool is_indirect_index_;
+    simd_expr_constraint constraint_;
 
-    explicit simdprint(Expression* expr): expr_(expr), is_var_indexed_(false), is_contiguous(false), is_constant(false) {}
+    explicit simdprint(Expression* expr): expr_(expr), is_indirect_index_(false),
+                                          constraint_(simd_expr_constraint ::other) {}
     explicit simdprint(Expression* expr, bool is_indexed):
-            expr_(expr), is_var_indexed_(is_indexed) {}
+            expr_(expr), is_indirect_index_(is_indexed) {}
 
-    void set_var_indexed() {
-        is_var_indexed_ = true;
+    void set_indirect_index() {
+        is_indirect_index_ = true;
     }
-    void set_contiguous() {
-        is_contiguous = true;
-    }
-    void set_constant() {
-        is_constant = true;
+    void set_constraint(simd_expr_constraint constraint) {
+        constraint_ = constraint;
     }
 
     friend std::ostream& operator<<(std::ostream& out, const simdprint& w) {
         SimdPrinter printer(out);
-        printer.set_var_indexed_to(w.is_var_indexed_);
-        printer.set_contiguous_to(w.is_contiguous);
-        printer.set_constant_to(w.is_constant);
+        printer.set_var_indexed_to(w.is_indirect_index_);
+        printer.set_constraint(w.constraint_);
         return w.expr_->accept(&printer), out;
     }
 };
@@ -450,11 +466,6 @@ static std::string index_i_name(const std::string& index_var) {
     return index_var+"i_";
 }
 
-static std::string index_constraint_name(const std::string& index_var) {
-    return index_var+"constraint_";
-}
-
-
 void SimdPrinter::visit(IdentifierExpression *e) {
     e->symbol()->accept(this);
 }
@@ -464,7 +475,7 @@ void SimdPrinter::visit(LocalVariable* sym) {
 }
 
 void SimdPrinter::visit(VariableExpression *sym) {
-    if(is_var_indexed_) {
+    if(is_indirect_index_) {
         out_ << "simd_value(" << sym->name() << "+index_)";
     }
     else if (sym->is_range()) {
@@ -486,7 +497,7 @@ void SimdPrinter::visit(AssignmentExpression* e) {
     if (lhs->is_variable() && lhs->is_variable()->is_range()) {
         out_ << "simd_value(";
         e->rhs()->accept(this);
-        if(is_var_indexed_)
+        if(is_indirect_index_)
             out_ << ").copy_to(" << lhs->name() << "+index_)";
         else
             out_ << ").copy_to(" << lhs->name() << "+i_)";
@@ -499,12 +510,12 @@ void SimdPrinter::visit(AssignmentExpression* e) {
 
 void SimdPrinter::visit(IndexedVariable *sym) {
     indexed_variable_info v = decode_indexed_variable(sym);
-    if(is_contiguous_) {
+    if(constraint_ == simd_expr_constraint::contiguous) {
         out_ << v.data_var
              << " + " << v.index_var
              << "[index_]";
     }
-    else if(is_constant_){
+    else if(constraint_ == simd_expr_constraint::constant){
         out_ << v.data_var
              << "[" << v.index_var
              << "element0]";
@@ -562,7 +573,7 @@ void emit_simd_state_read(std::ostream& out, LocalVariable* local, simdprint& pr
     out << "simd_value " << local->name();
 
     if (local->is_read()) {
-        if(printer.is_constant) {
+        if(printer.constraint_ == simd_expr_constraint::constant) {
             out << " = " << printer << ";\n";
         }
         else {
@@ -578,7 +589,7 @@ void emit_simd_state_update(std::ostream& out, Symbol* from, IndexedVariable* ex
     if (!external->is_write()) return;
 
     const char *op = external->op() == tok::plus ? " += " : " -= ";
-    if(printer.is_contiguous) {
+    if(printer.constraint_ == simd_expr_constraint::contiguous) {
         out << "simd_value t_"<< external->name() <<"(" << printer <<");\n";
         out << "t_" << external->name() << op << from->name() << ";\n";
         out << "t_" << external->name() << ".copy_to(" << printer << ");\n";
@@ -589,7 +600,6 @@ void emit_simd_state_update(std::ostream& out, Symbol* from, IndexedVariable* ex
     }
 }
 
-
 void emit_simd_api_body(std::ostream& out, APIMethod* method, moduleKind module_kind) {
     auto body = method->body();
     auto indexed_vars = indexed_locals(method->scope());
@@ -598,13 +608,6 @@ void emit_simd_api_body(std::ostream& out, APIMethod* method, moduleKind module_
     for (auto& sym: indexed_vars) {
         indices.insert(decode_indexed_variable(sym->external_variable()).index_var);
     }
-
-    // Note: expect to make index constraints non-constant for point mechanisms as
-    // an optimization in the near future.
-
-    // Another note: (TODO) can't actually use index_constraint::independent
-    // for density mechanisms because of collisions in the padded part of
-    // the indices. Work-arounds exist, but not yet implemented.
 
     if (!body->statements().empty())
         for (auto& index: indices) {
@@ -615,146 +618,116 @@ void emit_simd_api_body(std::ostream& out, APIMethod* method, moduleKind module_
         if (!indices.empty()) {
             out << "index_constraint constraint_category_;\n\n";
 
+            //Generate for loop for all contiguous simd_vectors
+            simd_expr_constraint constraint = simd_expr_constraint::contiguous;
+            std::string underlying_constraint = "contiguous";
 
-            {
-                out << "constraint_category_ = index_constraint::contiguous;\n";
-                out <<
-                    "for (unsigned i_ = 0; i_ < constraint_indices_.contiguous_indices.size(); i_++) {\n"
-                    << indent;
+            emit_for_loop_per_constraint(out, body, indexed_vars, indices, constraint,
+                                         constraint, underlying_constraint);
 
-                out << "unsigned index_ = constraint_indices_.contiguous_indices[i_];\n";
+            //Generate for loop for all independent simd_vectors
+            constraint = simd_expr_constraint::other;
+            underlying_constraint = "independent";
 
-                for (auto &sym: indexed_vars) {
-                    simdprint printer(sym->external_variable());
-                    printer.set_contiguous();
-                    emit_simd_state_read(out, sym, printer);
-                }
+            emit_for_loop_per_constraint(out, body, indexed_vars, indices, constraint,
+                                         constraint, underlying_constraint);
 
-                simdprint printer(body);
-                printer.set_var_indexed();
+            //Generate for loop for all simd_vectors that have no optimizing constraints
+            constraint = simd_expr_constraint::other;
+            underlying_constraint = "none";
 
-                out << printer;
+            emit_for_loop_per_constraint(out, body, indexed_vars, indices, constraint,
+                                         constraint, underlying_constraint);
 
-                for (auto &sym: indexed_vars) {
-                    simdprint printer(sym->external_variable());
-                    printer.set_contiguous();
-                    emit_simd_state_update(out, sym, sym->external_variable(), printer);
-                }
-                out << popindent << "}\n";
-            }
+            //Generate for loop for all constant simd_vectors
+            simd_expr_constraint read_constraint = simd_expr_constraint::constant;
+            simd_expr_constraint write_constraint = simd_expr_constraint::other;
+            underlying_constraint = "constant";
 
-            {
-                out << "constraint_category_ = index_constraint::independent;\n";
-                out <<
-                    "for (unsigned i_ = 0; i_ < constraint_indices_.independent_indices.size(); i_++) {\n"
-                    << indent;
+            emit_for_loop_per_constraint(out, body, indexed_vars, indices, read_constraint,
+                                         write_constraint, underlying_constraint);
 
-                out << "unsigned index_ = constraint_indices_.independent_indices[i_];\n";
-
-                for (auto &index: indices) {
-                    out << index_i_name(index) << ".copy_from(" << index << ".data() + index_);\n";
-                }
-
-                for (auto &sym: indexed_vars) {
-                    simdprint printer(sym->external_variable());
-                    emit_simd_state_read(out, sym, printer);
-                }
-
-                simdprint printer(body);
-                printer.set_var_indexed();
-
-                out << printer;
-
-                for (auto &sym: indexed_vars) {
-                    simdprint printer(sym->external_variable());
-                    emit_simd_state_update(out, sym, sym->external_variable(), printer);
-                }
-                out << popindent << "}\n";
-            }
-
-            {
-                out << "constraint_category_ = index_constraint::none;\n";
-                out <<
-                    "for (unsigned i_ = 0; i_ < constraint_indices_.serialized_indices.size(); i_++) {\n"
-                    << indent;
-
-                out << "unsigned index_ = constraint_indices_.serialized_indices[i_];\n";
-
-                for (auto &index: indices) {
-                    out << index_i_name(index) << ".copy_from(" << index << ".data() + index_);\n";
-                }
-
-                for (auto &sym: indexed_vars) {
-                    simdprint printer(sym->external_variable());
-                    emit_simd_state_read(out, sym, printer);
-
-                }
-
-                simdprint printer(body);
-                printer.set_var_indexed();
-
-                out << printer;
-
-                for (auto &sym: indexed_vars) {
-                    simdprint printer(sym->external_variable());
-                    emit_simd_state_update(out, sym, sym->external_variable(), printer);
-                }
-                out << popindent << "}\n";
-            }
-
-            {
-                out << "constraint_category_ = index_constraint::constant;\n";
-                out <<
-                    "for (unsigned i_ = 0; i_ < constraint_indices_.constant_indices.size() ; i_++) {\n"
-                    << indent;
-
-                out << "unsigned index_ = constraint_indices_.constant_indices[i_];\n";
-
-
-                for (auto &index: indices) {
-                    out << "simd_index::scalar_type " << index <<"element0 = " << index <<"[index_];\n";
-                    out << index_i_name(index) << " = " << index << "element0;\n";
-                }
-
-                for (auto &sym: indexed_vars) {
-                    simdprint printer(sym->external_variable());
-                    printer.set_constant();
-                    emit_simd_state_read(out, sym, printer);
-                }
-
-                simdprint printer(body);
-                printer.set_var_indexed();
-
-                out << printer;
-
-                for (auto &sym: indexed_vars) {
-                    simdprint printer(sym->external_variable());
-                    emit_simd_state_update(out, sym, sym->external_variable(), printer);
-                }
-                out << popindent << "}\n";
-            }
-
-        } else {
+        }
+        else {
             out << "unsigned n_ = width_;\n\n";
             out <<
                 "for (unsigned i_ = 0; i_ < n_; i_ += simd_width_) {\n" << indent;
-
-            for (auto &index: indices) {
-                out << index_i_name(index) << ".copy_from(" << index << ".data()+i_);\n";
-            }
-
-            for (auto &sym: indexed_vars) {
-                simdprint printer(sym->external_variable());
-                emit_simd_state_read(out, sym, printer);
-            }
-
             out << simdprint(body);
-
-            for (auto &sym: indexed_vars) {
-                simdprint printer(sym->external_variable());
-                emit_simd_state_update(out, sym, sym->external_variable(), printer);
-            }
             out << popindent << "}\n";
         }
+    }
+}
+
+void emit_for_loop_per_constraint(std::ostream &out, BlockExpression *body,
+                                  const std::vector<LocalVariable *> &indexed_vars,
+                                  const std::unordered_set<std::string> &indices,
+                                  const simd_expr_constraint &read_constraint,
+                                  const simd_expr_constraint &write_constraint,
+                                  std::string underlying_constraint_name) {
+
+    out << "constraint_category_ = index_constraint::"<< underlying_constraint_name << ";\n";
+    out << "for (unsigned i_ = 0; i_ < index_constraints_." << underlying_constraint_name
+        << ".size(); i_++) {\n"
+        << indent;
+
+    out << "index_type index_ = index_constraints_." << underlying_constraint_name << "[i_];\n";
+
+    emit_body_for_loop(out, body, indexed_vars, indices, read_constraint, write_constraint);
+
+    out << popindent << "}\n";
+}
+
+void emit_body_for_loop(std::ostream &out, BlockExpression *body, const std::vector<LocalVariable *> &indexed_vars,
+                   const std::unordered_set<std::string> &indices, const simd_expr_constraint &read_constraint,
+                   const simd_expr_constraint &write_constraint) {
+    emit_index_initialize(out, indices, read_constraint);
+
+    emit_indexed_simd_state_reads(out, indexed_vars, read_constraint);
+
+    simdprint printer(body);
+    printer.set_indirect_index();
+
+    out << printer;
+
+    emit_indexed_simd_state_updates(out, indexed_vars, write_constraint);
+}
+
+void emit_index_initialize(std::ostream &out, const std::unordered_set<std::string> &indices,
+                           simd_expr_constraint constraint) {
+    switch(constraint) {
+        case simd_expr_constraint::contiguous :
+            break;
+        case simd_expr_constraint::constant : {
+            for (auto &index: indices) {
+                out << "simd_index::scalar_type " << index << "element0 = " << index << "[index_];\n";
+                out << index_i_name(index) << " = " << index << "element0;\n";
+            }
+        }
+        break;
+        case simd_expr_constraint::other : {
+            for (auto &index: indices) {
+                out << index_i_name(index) << ".copy_from(" << index << ".data() + index_);\n";
+            }
+        }
+        break;
+
+    }
+}
+
+void emit_indexed_simd_state_updates(std::ostream &out, const std::vector<LocalVariable *> &indexed_vars,
+                                     const simd_expr_constraint &constraint) {
+    for (auto &sym: indexed_vars) {
+        simdprint printer(sym->external_variable());
+        printer.set_constraint(constraint);
+        emit_simd_state_update(out, sym, sym->external_variable(), printer);
+    }
+}
+
+void emit_indexed_simd_state_reads(std::ostream &out, const std::vector<LocalVariable *> &indexed_vars,
+                                   simd_expr_constraint constraint) {
+    for (auto &sym: indexed_vars) {
+        simdprint printer(sym->external_variable());
+        printer.set_constraint(constraint);
+        emit_simd_state_read(out, sym, printer);
     }
 }
--- tests/unit/test_partition_by_constraint.cpp
+++ tests/unit/test_partition_by_constraint.cpp
@@ -28,15 +28,15 @@ TEST(partition_by_constraint, partition_contiguous) {
             expected_indices.push_back(i);
     }
 
-    multicore::gen_constraint(input_index, output_constraint, input_size_);
+    multicore::generate_index_constraint_partitions(input_index, output_constraint, input_size_);
 
-    EXPECT_EQ(input_size_/simd_width_, output_constraint.contiguous_indices.size());
-    EXPECT_EQ(0, output_constraint.constant_indices.size());
-    EXPECT_EQ(0, output_constraint.independent_indices.size());
-    EXPECT_EQ(0, output_constraint.serialized_indices.size());
+    EXPECT_EQ(input_size_/simd_width_, output_constraint.contiguous.size());
+    EXPECT_EQ(0, output_constraint.constant.size());
+    EXPECT_EQ(0, output_constraint.independent.size());
+    EXPECT_EQ(0, output_constraint.none.size());
 
     for (unsigned i = 0; i < input_size_ / simd_width_; i++) {
-        EXPECT_EQ(expected_indices[i], output_constraint.contiguous_indices[i]);
+        EXPECT_EQ(expected_indices[i], output_constraint.contiguous[i]);
     }
 }
 
@@ -53,24 +53,24 @@ TEST(partition_by_constraint, partition_constant) {
             expected_indices.push_back(i);
     }
 
-    multicore::gen_constraint(input_index, output_constraint, input_size_);
+    multicore::generate_index_constraint_partitions(input_index, output_constraint, input_size_);
 
     if(simd_width_ != 1) {
-        EXPECT_EQ(input_size_/simd_width_, output_constraint.constant_indices.size());
-        EXPECT_EQ(0, output_constraint.contiguous_indices.size());
+        EXPECT_EQ(input_size_/simd_width_, output_constraint.constant.size());
+        EXPECT_EQ(0, output_constraint.contiguous.size());
     }
     else {
-        EXPECT_EQ(0, output_constraint.constant_indices.size());
-        EXPECT_EQ(input_size_/simd_width_, output_constraint.contiguous_indices.size());
+        EXPECT_EQ(0, output_constraint.constant.size());
+        EXPECT_EQ(input_size_/simd_width_, output_constraint.contiguous.size());
     }
-    EXPECT_EQ(0, output_constraint.independent_indices.size());
-    EXPECT_EQ(0, output_constraint.serialized_indices.size());
+    EXPECT_EQ(0, output_constraint.independent.size());
+    EXPECT_EQ(0, output_constraint.none.size());
 
     for (unsigned i = 0; i < input_size_ / simd_width_; i++) {
         if(simd_width_ != 1)
-            EXPECT_EQ(expected_indices[i], output_constraint.constant_indices[i]);
+            EXPECT_EQ(expected_indices[i], output_constraint.constant[i]);
         else
-            EXPECT_EQ(expected_indices[i], output_constraint.contiguous_indices[i]);
+            EXPECT_EQ(expected_indices[i], output_constraint.contiguous[i]);
     }
 }
 
@@ -85,24 +85,24 @@ TEST(partition_by_constraint, partition_independent) {
             expected_indices.push_back(i);
     }
 
-    multicore::gen_constraint(input_index, output_constraint, input_size_);
+    multicore::generate_index_constraint_partitions(input_index, output_constraint, input_size_);
 
     if(simd_width_ != 1) {
-        EXPECT_EQ(input_size_/simd_width_, output_constraint.independent_indices.size());
-        EXPECT_EQ(0, output_constraint.contiguous_indices.size());
+        EXPECT_EQ(input_size_/simd_width_, output_constraint.independent.size());
+        EXPECT_EQ(0, output_constraint.contiguous.size());
     }
     else {
-        EXPECT_EQ(0, output_constraint.independent_indices.size());
-        EXPECT_EQ(input_size_/simd_width_, output_constraint.contiguous_indices.size());
+        EXPECT_EQ(0, output_constraint.independent.size());
+        EXPECT_EQ(input_size_/simd_width_, output_constraint.contiguous.size());
     }
-    EXPECT_EQ(0, output_constraint.constant_indices.size());
-    EXPECT_EQ(0, output_constraint.serialized_indices.size());
+    EXPECT_EQ(0, output_constraint.constant.size());
+    EXPECT_EQ(0, output_constraint.none.size());
 
     for (unsigned i = 0; i < input_size_ / simd_width_; i++) {
         if(simd_width_ != 1)
-            EXPECT_EQ(expected_indices[i], output_constraint.independent_indices[i]);
+            EXPECT_EQ(expected_indices[i], output_constraint.independent[i]);
         else
-            EXPECT_EQ(expected_indices[i], output_constraint.contiguous_indices[i]);
+            EXPECT_EQ(expected_indices[i], output_constraint.contiguous[i]);
     }
 }
 
@@ -117,24 +117,24 @@ TEST(partition_by_constraint, partition_serial) {
             expected_indices.push_back(i);
     }
 
-    multicore::gen_constraint(input_index, output_constraint, input_size_);
+    multicore::generate_index_constraint_partitions(input_index, output_constraint, input_size_);
 
     if(simd_width_ != 1) {
-        EXPECT_EQ(input_size_/simd_width_, output_constraint.serialized_indices.size());
-        EXPECT_EQ(0, output_constraint.contiguous_indices.size());
+        EXPECT_EQ(input_size_/simd_width_, output_constraint.none.size());
+        EXPECT_EQ(0, output_constraint.contiguous.size());
     }
     else {
-        EXPECT_EQ(0, output_constraint.serialized_indices.size());
-        EXPECT_EQ(input_size_/simd_width_, output_constraint.contiguous_indices.size());
+        EXPECT_EQ(0, output_constraint.none.size());
+        EXPECT_EQ(input_size_/simd_width_, output_constraint.contiguous.size());
     }
-    EXPECT_EQ(0, output_constraint.independent_indices.size());
-    EXPECT_EQ(0, output_constraint.constant_indices.size());
+    EXPECT_EQ(0, output_constraint.independent.size());
+    EXPECT_EQ(0, output_constraint.constant.size());
 
     for (unsigned i = 0; i < input_size_ / simd_width_; i++) {
         if(simd_width_ != 1)
-            EXPECT_EQ(expected_indices[i], output_constraint.serialized_indices[i]);
+            EXPECT_EQ(expected_indices[i], output_constraint.none[i]);
         else
-            EXPECT_EQ(expected_indices[i], output_constraint.contiguous_indices[i]);
+            EXPECT_EQ(expected_indices[i], output_constraint.contiguous[i]);
     }
 }
 
@@ -161,32 +161,32 @@ TEST(partition_by_constraint, partition_random) {
             expected_indices_contiguous.push_back(i);
     }
 
-    multicore::gen_constraint(input_index, output_constraint, input_size_);
+    multicore::generate_index_constraint_partitions(input_index, output_constraint, input_size_);
 
     if (simd_width_ != 1) {
-        EXPECT_EQ(input_size_ / 4 / simd_width_, output_constraint.contiguous_indices.size());
-        EXPECT_EQ(input_size_ / 4 / simd_width_, output_constraint.constant_indices.size());
-        EXPECT_EQ(input_size_ / 4 / simd_width_, output_constraint.independent_indices.size());
-        EXPECT_EQ(input_size_ / 4 / simd_width_, output_constraint.serialized_indices.size());
+        EXPECT_EQ(input_size_ / 4 / simd_width_, output_constraint.contiguous.size());
+        EXPECT_EQ(input_size_ / 4 / simd_width_, output_constraint.constant.size());
+        EXPECT_EQ(input_size_ / 4 / simd_width_, output_constraint.independent.size());
+        EXPECT_EQ(input_size_ / 4 / simd_width_, output_constraint.none.size());
     }
     else {
-        EXPECT_EQ(input_size_ / simd_width_, output_constraint.contiguous_indices.size());
-        EXPECT_EQ(0, output_constraint.constant_indices.size());
-        EXPECT_EQ(0, output_constraint.independent_indices.size());
-        EXPECT_EQ(0, output_constraint.serialized_indices.size());
+        EXPECT_EQ(input_size_ / simd_width_, output_constraint.contiguous.size());
+        EXPECT_EQ(0, output_constraint.constant.size());
+        EXPECT_EQ(0, output_constraint.independent.size());
+        EXPECT_EQ(0, output_constraint.none.size());
     }
 
     if (simd_width_ != 1) {
         for (unsigned i = 0; i < input_size_ / simd_width_ / 4; i++) {
-            EXPECT_EQ(expected_indices_contiguous[i], output_constraint.contiguous_indices[i]);
-            EXPECT_EQ(expected_indices_constant[i], output_constraint.constant_indices[i]);
-            EXPECT_EQ(expected_indices_independent[i], output_constraint.independent_indices[i]);
-            EXPECT_EQ(expected_indices_serial[i], output_constraint.serialized_indices[i]);
+            EXPECT_EQ(expected_indices_contiguous[i], output_constraint.contiguous[i]);
+            EXPECT_EQ(expected_indices_constant[i], output_constraint.constant[i]);
+            EXPECT_EQ(expected_indices_independent[i], output_constraint.independent[i]);
+            EXPECT_EQ(expected_indices_serial[i], output_constraint.none[i]);
         }
     }
     else {
         for (unsigned i = 0; i < input_size_ / simd_width_; i++) {
-            EXPECT_EQ(i, output_constraint.contiguous_indices[i]);
+            EXPECT_EQ(i, output_constraint.contiguous[i]);
         }
     }
 }
