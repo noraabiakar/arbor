f2cec8efae080fe131176add430af9a841aa6ee3
abiakarn <nora.abiakar@gmail.com>
2018-08-06 10:46:11 +0200

    dynamic array of cudaStream

--- include/arbor/gpu_context.hpp
+++ include/arbor/gpu_context.hpp
@@ -21,7 +21,7 @@ struct gpu_context {
     bool has_gpu_;
     size_t attributes_ = 0;
 #ifdef ARB_GPU_ENABLED
-    cudaStream_t streams_[8];
+    cudaStream_t* streams_;
 #endif
 
     gpu_context(): has_gpu_(false), attributes_(get_attributes()) {
@@ -31,6 +31,12 @@ struct gpu_context {
         set_cuda_streams();
     };
 
+#ifdef ARB_GPU_ENABLED
+    ~gpu_context() {
+        delete[] streams_;
+    }
+#endif
+
 private:
     size_t get_attributes() {
         size_t attributes = 0;
@@ -38,7 +44,6 @@ private:
         cudaDeviceProp prop;
         cudaGetDeviceProperties(&prop, 0);
         if(prop.concurrentManagedAccess)
-        //if(prop.major*100 + prop.minor >= 600)
             attributes|= (1<<gpu_flags::no_sync);
         if(prop.major*100 + prop.minor >= 600)
             attributes|= (1<<gpu_flags::has_atomic_double);
@@ -46,9 +51,10 @@ private:
         return attributes;
     };
 
-    void set_cuda_streams() {
+    void set_cuda_streams(unsigned size = 4) {
 #ifdef ARB_GPU_ENABLED
-        for (unsigned i = 0; i < 8; i++) {
+        streams_ = new cudaStream_t[size];
+        for (unsigned i = 0; i < size; i++) {
             cudaStreamCreate(&streams_[i]);
         }
 #endif
