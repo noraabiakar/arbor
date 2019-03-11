87190f6b27235df597998ad4a352e6b4d5416fba
abiakarn <nora.abiakar@gmail.com>
2019-03-11 17:02:26 +0100

    building cell done, building edge tables done

--- example/sonata/sonata.cpp
+++ example/sonata/sonata.cpp
@@ -43,13 +43,16 @@ arb::cable_cell dummy_cell(
         std::vector<std::pair<arb::segment_location, double>>,
         std::vector<std::pair<arb::segment_location, arb::mechanism_desc>>);
 
-class sonata_recipe: public arb::recipe {
+
+struct edge_info{
+    std::pair<unsigned, unsigned> source;
+    std::pair<unsigned, unsigned> target;
+    unsigned type;
+};
+
+class database {
 public:
-    sonata_recipe(record nodes, record edges):
-            nodes_(nodes),
-            edges_(edges),
-            num_cells_(nodes_.num_elements())
-    {
+    database(record nodes, record edges): nodes_(nodes), edges_(edges) {
         edge_tables.reserve(edges.populations().size());
         for (unsigned i = 0; i < edges.populations().size(); i++) {
             edge_tables.emplace_back(edges.partitions()[i]);
@@ -60,13 +63,25 @@ public:
         for(auto n: nodes_.partitions()) {
             gid_partition.push_back(sum+=n);
         }
+
+        arb::mechanism_desc expsyn("expsyn");
+        arb::mechanism_desc exp2syn("exp2syn");
+        type_to_conn_info[100] = conn_info(arb::segment_location{0, 0.1}, arb::segment_location{1, 0.2}, 0.5, expsyn);
+        type_to_conn_info[101] = conn_info(arb::segment_location{1, 0.1}, arb::segment_location{0, 0.1}, 0.1, expsyn);
+        type_to_conn_info[102] = conn_info(arb::segment_location{0, 0.5}, arb::segment_location{0, 0.5}, 0.3, exp2syn);
     }
 
-    cell_size_type num_cells() const override {
-        return num_cells_;
+    cell_size_type num_cells() {
+        return nodes_.num_elements();
     }
 
-    arb::util::unique_any get_cell_description(cell_gid_type gid) const override {
+    cell_size_type num_edges() {
+        return edges_.num_elements();
+    }
+
+    void get_sources_and_targets(cell_gid_type gid,
+            std::vector<std::pair<arb::segment_location, double>>& src,
+            std::vector<std::pair<arb::segment_location, arb::mechanism_desc>>& tgt) {
         unsigned i = 0;
         for (; i < gid_partition.size(); i++) {
             if (gid < gid_partition[i]) {
@@ -76,7 +91,6 @@ public:
 
         unsigned node_pop = i-1;
         unsigned pop_loc = gid - gid_partition[node_pop];
-        std::cout << node_pop << " " << pop_loc << std::endl;
 
         // Replace with CSV read
         std::vector<unsigned> target_pops;
@@ -94,17 +108,18 @@ public:
             range.first = edges_[tp][indices_id][target_to_source_id].dataset_at("node_id_to_ranges", pop_loc, 0).value();
             range.second = edges_[tp][indices_id][target_to_source_id].dataset_at("node_id_to_ranges", pop_loc, 1).value();
 
-            std::cout << "\ttarget range: " << range.first << " " << range.second << std::endl;
-
             for (unsigned i = range.first; i < range.second; i++) {
                 std::pair<unsigned, unsigned> edge_ids;
                 edge_ids.first = edges_[tp][indices_id][target_to_source_id].dataset_at("range_to_edge_id", i, 0).value();
                 edge_ids.second = edges_[tp][indices_id][target_to_source_id].dataset_at("range_to_edge_id", i, 1).value();
-                std::cout << "\ttarget edges: " << edge_ids.first << " " << edge_ids.second << std::endl;
 
-                for (unsigned j = edge_ids.first; j < edge_ids.second; j++) {
+                unsigned loc_idx = 0;
+                for (unsigned j = edge_ids.first; j < edge_ids.second; j++, loc_idx++) {
                     auto type = edges_[tp].dataset_at("edge_type_id", j).value();
-                    std::cout << "\t\tedge: " << j << " is type: " << type << std::endl;
+                    edge_tables[tp][j].target = {gid, loc_idx};
+                    edge_tables[tp][j].type = type;
+                    auto conn_info = type_to_conn_info[type];
+                    tgt.push_back(std::make_pair(conn_info.tgt_, conn_info.syn_));
                 }
             }
         }
@@ -124,22 +139,77 @@ public:
             range.first = edges_[sp][indices_id][source_to_target_id].dataset_at("node_id_to_ranges", pop_loc, 0).value();
             range.second = edges_[sp][indices_id][source_to_target_id].dataset_at("node_id_to_ranges", pop_loc, 1).value();
 
-            std::cout << "\tsource range: " << range.first << " " << range.second << std::endl;
-
             for (unsigned i = range.first; i < range.second; i++) {
                 std::pair<unsigned, unsigned> edge_ids;
                 edge_ids.first = edges_[sp][indices_id][source_to_target_id].dataset_at("range_to_edge_id", i, 0).value();
                 edge_ids.second = edges_[sp][indices_id][source_to_target_id].dataset_at("range_to_edge_id", i, 1).value();
-                std::cout << "\tsource edges: " << edge_ids.first << " " << edge_ids.second << std::endl;
 
-                for (unsigned j = edge_ids.first; j < edge_ids.second; j++) {
+                unsigned loc_idx = 0;
+                for (unsigned j = edge_ids.first; j < edge_ids.second; j++, loc_idx++) {
                     auto type = edges_[sp].dataset_at("edge_type_id", j).value();
-                    std::cout << "\t\tedge: " << j << " is type: " << type << std::endl;
+                    edge_tables[sp][j].source = {gid, loc_idx};
+                    edge_tables[sp][j].type = type;
+                    auto conn_info = type_to_conn_info[type];
+                    src.push_back(std::make_pair(conn_info.src_, conn_info.weight_));
                 }
             }
         }
+    }
 
-        return dummy_cell({}, {});
+    void print_tables() {
+        int n= 0;
+        for(auto e: edge_tables) {
+            std::cout << n++ << ":" << std::endl;
+            for(auto entry: e) {
+                std::cout << "\t{ [" << entry.source.first << ", " << entry.source.second << "], ["
+                          << entry.target.first << ", " << entry.target.second << "], " << entry.type << " }" <<std::endl;
+            }
+        }
+    }
+
+private:
+
+    record nodes_;
+    record edges_;
+    std::vector<std::vector<edge_info>> edge_tables;
+    std::vector<cell_size_type> gid_partition;
+
+    struct conn_info {
+        conn_info() : src_(0,0), tgt_(0,0), weight_(0), syn_("expsyn") {};
+        conn_info (
+            arb::segment_location src,
+            arb::segment_location tgt,
+            double weight,
+            arb::mechanism_desc syn):
+            src_(src), tgt_(tgt), weight_(weight), syn_(syn) {};
+
+        arb::segment_location src_;
+        arb::segment_location tgt_;
+        double weight_;
+        arb::mechanism_desc syn_;
+    };
+    std::unordered_map<cell_size_type, conn_info> type_to_conn_info;
+
+};
+
+class sonata_recipe: public arb::recipe {
+public:
+    sonata_recipe(record nodes, record edges):
+            database_(nodes, edges),
+            num_cells_(database_.num_cells()) {}
+
+    cell_size_type num_cells() const override {
+        return num_cells_;
+    }
+
+    arb::util::unique_any get_cell_description(cell_gid_type gid) const override {
+
+        std::vector<std::pair<arb::segment_location,double>> src_types;
+        std::vector<std::pair<arb::segment_location,arb::mechanism_desc>> tgt_types;
+
+        database_.get_sources_and_targets(gid, src_types, tgt_types);
+
+        return dummy_cell(src_types, tgt_types);
     }
 
     cell_kind get_cell_kind(cell_gid_type gid) const override { return cell_kind::cable; }
@@ -153,20 +223,14 @@ public:
     // Each cell has one incoming connection, from cell with gid-1.
     std::vector<arb::cell_connection> connections_on(cell_gid_type gid) const override { return {}; }
 
+    void print_tables() {
+        database_.print_tables();
+    }
+
 
 private:
-    struct edge_info{
-        std::pair<unsigned, unsigned> source;
-        std::pair<unsigned, unsigned> target;
-        unsigned type;
-    };
-
-    record nodes_;
-    record edges_;
+    mutable database database_;
     cell_size_type num_cells_;
-    std::vector<cell_size_type> gid_partition;
-
-    std::vector<std::vector<edge_info>> edge_tables;
 };
 
 void print(const std::shared_ptr<h5_file>& file) {
@@ -248,8 +312,19 @@ int main(int argc, char **argv)
     std::cout << "Number of cells = " << recipe.num_cells() << std::endl;
 
     for(unsigned i =0; i < 12; i++) {
-        recipe.get_cell_description(i);
+        std::cout << "Cell " << i << std::endl;
+        auto cell = arb::util::any_cast<arb::cable_cell>(recipe.get_cell_description(i));
+        std::cout << "Synapses: " << std::endl;
+        for (auto s: cell.synapses()) {
+            std::cout << "\t" << s.location.segment << ", " << s.location.position << ": " << s.mechanism.name() <<std::endl;
+        }
+        std::cout << "Detectors: " << std::endl;
+        for (auto d: cell.detectors()) {
+            std::cout << "\t" << d.location.segment << ", " << d.location.position << ": " << d.threshold <<std::endl;
+        }
     }
+
+    recipe.print_tables();
 
     return 0;
 }
