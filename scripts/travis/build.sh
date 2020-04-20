e7265ce50ff5b89a2a207797b8e6a70206de99d6
abiakarn <nora.abiakar@gmail.com>
2020-04-20 13:42:15 +0200

    fix and try another test

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -14,8 +14,7 @@ if [[ "${CHECK_FORMAT}" = "true" ]]; then
   if [ "$output" == "no modified files to format" ] || [ "$output" == "clang-format did not modify any files" ] ; then
     success "clang-format passed"
   else
-    echo -e "${RED}clang-format suggested diff${CLEAR}:
-    echo "$output" || error "clang-format failed:"
+    echo "$output" || error "clang-format failed"
   fi
 fi
 
